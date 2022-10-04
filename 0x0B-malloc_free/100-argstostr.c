99827098c029440af7ac275e1d46e2a331caa09c
Alausa <alausaabdulazeez@gmail.com>
2022-10-04 02:59:42 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -88,134 +88,67 @@ char **strtow(char *str)
 	return (strArr);
 }
 
-
-
 /**
-
  * word_count - Count number of words
-
  *
-
  * @str: char pointer
-
  *
-
  * Return: Word count
-
  */
-
-
 
 int word_count(char *str)
-
 {
-
 	int counter = 0, wordCount, letter;
 
-
-
 	while (str[counter] != '\0')
-
 	{
-
 		if (str[counter] != ' ' && !letter)
-
 		{
-
 			wordCount++;
-
 			letter = 1;
-
 		}
-
 		else if (str[counter] == ' ' && letter)
-
 		{
-
 			letter = 0;
-
 		}
-
 		counter++;
-
 	}
-
 	return (wordCount);
-
 }
 
-
-
-
-
 /**
-
  * first_word - Gets first word
-
  * @str: char pointer
-
  * Return: Pointer to word
-
  */
 
-
-
 char *first_word(char *str)
-
 {
-
 	int counter;
-
 	char *word;
-
-
 
 	counter = 0;
 
-
-
 	while (str[counter] != ' ' && str[counter] != '\0')
-
 	{
-
 		counter++;
-
 	}
-
-
 
 	word = malloc(sizeof(char) * (counter + 1));
 
-
-
 		if (!word)
-
 		{
-
 			return (NULL);
-
 		}
-
-
 
 		word[counter] = '\0';
 
-
-
 		counter--;
 
-
-
 		while (counter >= 0)
-
 		{
-
 			word[counter] = str[counter];
-
 			counter--;
-
 		}
-
 		return (word);
-
 }
