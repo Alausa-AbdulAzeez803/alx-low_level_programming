e7f182fb6faf2a825e6d1e59b2c497912322f056
Alausa <alausaabdulazeez@gmail.com>
2022-10-12 08:40:43 -0700

    opp

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,73 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
-/**
- * op_add - function that return sum a + b
- * @a: first value
- * @b: second value
- *
- * Return: suma a+ b
- */
-
-int op_add(int a, int b)
-{
-	return (a + b);
-}
+#include <string.h>
 
 /**
- * op_sub - function that return sub a - b
- * @a: first value
- * @b: second value
+ * get_op_func - function that selects the operation asked by the user.
+ * @s: operator
  *
- * Return: sub a - b
+ * Return: result
  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
 
-/**
- * op_mul - function that return mul a * b
- * @a: first value
- * @b: second value
- *
- * Return: mul a * b
- */
-int op_mul(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a * b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-/**
-* op_div - function that return div a / b
- * @a: first value
- * @b: second value
- *
- * Return: div a / b
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(100);
+		if (!(strcmp(ops[i].op, s)))
+			return (ops[i].f);
+		i++;
 	}
-	return (a / b);
-}
-
-/**
- * op_mod - function that return mod a % b
- * @a: first value
- * @b: second value
- *
- * Return: mod a % b
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+	return (NULL);
 }
