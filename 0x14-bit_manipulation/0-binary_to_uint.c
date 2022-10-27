14cc94689b86ac3c9c7da6cc15d0194cee68c9ac
Alausa <alausaabdulazeez@gmail.com>
2022-10-27 09:36:18 -0700

    uni

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /**
  * binary_to_uint - convert binary to decimal
  * @b: unsigned char to be converted to decimal
  * Return: converted number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int base = 1, rem, decimal = 0, len, count = 0;
-
 
 	if (!b)
 		return (0);
